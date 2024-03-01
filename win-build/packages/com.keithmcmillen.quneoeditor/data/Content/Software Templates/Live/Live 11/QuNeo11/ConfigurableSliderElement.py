111d655cab1f458b228cd27f072f6e51236028d7
Eric Bateman <eric@keithmcmillen.com>
2024-03-01 10:01:48 -0800

    2.0.3 Release -Windows now builds installer as part of PRO file, same as 12 Step -SSL update checks aren't exactly working on Windows, need to test this in SoftStep and port back the fixes

---
+++ win-build/packages/com.keithmcmillen.quneoeditor/data/Content/Software Templates/Live/Live 11/QuNeo11/ConfigurableSliderElement.py
@@ -0,0 +1,16 @@
+import Live
+
+from _Framework.SliderElement import *
+
+class ConfigurableSliderElement(SliderElement):
+	__module__ = __name__
+	__doc__ = ' Special Slider class that can be configured with custom on- and off-values '
+
+	def __init__(self, msg_type, channel, identifier):
+		ButtonElement.__init__(self, msg_type, channel, identifier)
+
+	def send_value(self,value):
+		if(value != self._last_active_value):
+			ButtonElement.send_value(self, value, (force or self._force_next_value))
+		self._last_active_value = value
+		self._force_next_value = False
